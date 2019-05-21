d3409ae2a7d3101eff0cfae5e024541a8c24a800
NiravSo <45560926+NiravSo@users.noreply.github.com>
2019-05-21 12:04:34 +0530

    vregeg

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var http = require('http');
 var handleRequest = function (request, response) {
   response.writeHead(200);
   
-  response.end('I have commited new code!');
+  response.end('Welcome, Its Blue Green Deployment');
   
 };
 var www = http.createServer(handleRequest);
